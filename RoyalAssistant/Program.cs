93a29f40fd31027684ccd18bf05ece7df0cff706
princer007 <princer007@mail.ru>
2014-12-02 00:26:06 +0200

    Some new features

--- RoyalAssistant/Program.cs
+++ RoyalAssistant/Program.cs
@@ -2,6 +2,7 @@
 using LeagueSharp;
 using LeagueSharp.Common;
 using System.Collections.Generic;
+using System.Diagnostics;
 using SharpDX;
 
 using Color = System.Drawing.Color;
@@ -14,10 +15,10 @@ namespace RoyalAssistant
         static int[] SRExpCumulative = { 0, 280, 660, 1140, 1720, 2400, 3180, 4060, 5040, 6120, 7300, 8580, 9960, 11440, 13020, 14700, 16480, 18360 };
         static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
 
-        private static void Game_OnGameLoad(EventArgs args)
+        static void OnGameLoad(EventArgs args)
         {
             LoadMenu();
             
@@ -28,11 +29,14 @@ namespace RoyalAssistant
             }
 
             Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameEnd += OnGameEnd;
             Console.WriteLine("RoyalAssistant Loaded!");
         }
 
-        private static void Drawing_OnDraw(EventArgs args)
+        static void Drawing_OnDraw(EventArgs args)
         {
+            if (Utility.InShopRange() && menu.Item("ward").GetValue<bool>() && !HasWard())
+                Drawing.DrawText(400, 600, GetColor(), "Buy a ward, save your life!");
             foreach (Obj_AI_Hero hero in ObjectManager.Get<Obj_AI_Hero>())
                 if (hero.Level != 18 && hero.IsVisible && !hero.IsDead)
                 {
@@ -83,7 +87,61 @@ namespace RoyalAssistant
 
         }
 
-        private static void LoadMenu()
+        static void OnGameEnd(EventArgs args)
+        {
+            Process.GetProcessesByName("League of Legends")[0].Kill();
+        }
+
+        static bool HasWard()
+        {
+            foreach (InventorySlot slot in ObjectManager.Player.InventoryItems)
+                if (slot.Name.ToLower().Contains("ward") && !slot.Name.ToLower().Contains("trinket"))
+                    return true;
+            return false;
+        }
+        
+        static Color GetColor()
+        {
+            switch ((int)((Game.Time % 1) * 10))//SHITTY CODE! :D
+            {
+                case 0:
+                    return Color.IndianRed;
+                    break;
+                case 1:
+                    return Color.LightGoldenrodYellow;
+                    break;
+                case 2:
+                    return Color.Goldenrod;
+                    break;
+                case 3:
+                    return Color.Green;
+                    break;
+                case 4:
+                    return Color.Blue;
+                    break;
+                case 5:
+                    return Color.Violet;
+                    break;
+                case 6:
+                    return Color.DeepPink;
+                    break;
+                case 7:
+                    return Color.DeepSkyBlue;
+                    break;
+                case 8:
+                    return Color.White;
+                    break;
+                case 9:
+                    return Color.Cyan;
+                    break;
+                default:
+                    return Color.ForestGreen;
+                    break;
+            }
+            return Color.Black;
+        }
+
+        static void LoadMenu()
         {
             // Initialize the menu
             menu = new Menu("RoyalAssistant", "RoyalAssistant", true);
@@ -95,6 +153,9 @@ namespace RoyalAssistant
             //menu.AddItem(new MenuItem("showSelfT", "Tracker showing self").SetValue(false));
             menu.AddItem(new MenuItem("showAlliesT", "Tracker showing allies").SetValue(true));
             menu.AddItem(new MenuItem("showEnemiesT", "Tracker showing enemies").SetValue(true));
+            menu.AddItem(new MenuItem("2", "                       Helper:"));
+            menu.AddItem(new MenuItem("end", "Quit game on end").SetValue(true));
+            menu.AddItem(new MenuItem("ward", "Show \"Buy ward\" reminder").SetValue(true));
             menu.AddToMainMenu();
             Console.WriteLine("Menu finalized");
         }
